8f9490fa175a9eaab41ad95bffdce2a4f69dce7b
Zijlmans, Paul (P) <paul.zijlmans@politie.nl>
2023-06-09 17:13:55 +0200

    Reactive Data Modification

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { filter, tap } from "rxjs/operators";
 
 @Component({
   selector: "courses-card-list",
   templateUrl: "./courses-card-list.component.html",
-  styleUrls: ["./courses-card-list.component.scss"],
+  styleUrls: ["./courses-card-list.component.css"],
 })
 export class CoursesCardListComponent {
   @Input()
   courses: Course[] = [];
+
+  @Output()
+  private coursesChanged = new EventEmitter();
 
   constructor(private dialog: MatDialog) {}
 
@@ -24,5 +28,12 @@ export class CoursesCardListComponent {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => this.coursesChanged.emit())
+      )
+      .subscribe();
   }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
 import { Course } from "../model/course";
 
@@ -17,5 +17,11 @@ export class CoursesService {
       map((res) => res.payload),
       shareReplay()
     );
+  }
+
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return this.http
+      .put(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
   }
 }
