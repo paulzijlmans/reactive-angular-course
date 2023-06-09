e98936876e95245a157d6d9fce24e203ecc3fdc0
Zijlmans, Paul (P) <paul.zijlmans@politie.nl>
2023-06-09 16:36:04 +0200

    Stateless Observable-based Services

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Course } from "../model/course";
+
+type CoursesResponse = {
+  payload: Course[];
+};
+
+@Injectable({ providedIn: "root" })
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http
+      .get<CoursesResponse>("/api/courses")
+      .pipe(map((res) => res.payload));
+  }
+}
